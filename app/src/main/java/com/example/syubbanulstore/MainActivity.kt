70d0dea815fff95879d6ac7914973d4b8e5a65d1
Mustaq1002 <82882446+Mustaq1002@users.noreply.github.com>
2021-04-21 13:18:09 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/syubbanulstore/MainActivity.kt
+++ app/src/main/java/com/example/syubbanulstore/MainActivity.kt
@@ -4,17 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    
-    val fargmentHome: = Fragment = HomeFragment()
-    val fargmentAkun: = Fragment = AkunFragment()
-    val fargmentKeranjang: = Fragment = KeranjangFragment()
-    val fm: FragmentManager = supportFragmentManager
-    var active: Fragment = fargmentHome
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        fm.beginTransaction().add(R.id.container, fargmentHome).show(fargmentHome).commit()
     }
 }
